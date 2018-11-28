8fb708e6ecc619da8e55d343541ba9dec0548645
Markus <m.olt@blaumond.net>
2018-11-28 10:41:57 +0100

    Added new instructions for if statements and loading of text and json files.

--- source/spp/Instruction.cs
+++ source/spp/Instruction.cs
@@ -22,7 +22,11 @@ namespace Spp {
 			{"cdoutput", new Instruction(_cdoutput, 0, 1)},
 			{"close",    new Instruction(_close,    0, 0)},
 			{"for",      new Instruction(_for,      1, 2)},
-			{"add",      new Instruction(_add,      0, 2)}
+			{"add",      new Instruction(_add,      0, 2)},
+			{"if",       new Instruction(_if,       0, 2)},
+			{"always",   new Instruction(_always,   0, 0)},
+			{"never",    new Instruction(_never,    0, 0)},
+			{"equals",   new Instruction(_equals,   0, 2)}
 		};
 
 		internal Instruction (Func<Compiler, Variable[], ValueRecipe[], Value> function, int variableCount, int valueCount) {
@@ -74,6 +78,20 @@ namespace Spp {
 			return Value.Empty;
 		}
 
+		private static string _resolvePath (string basePath, string filePath, Position position, bool canCreate) {
+			try {
+				filePath = Path.Combine(basePath, filePath);
+			} catch (ArgumentException e) {
+				throw new CompileException("Illegal characters in path.", position, e);
+			}
+
+			if (!canCreate && !File.Exists(filePath)) {
+				throw new CompileException("File does not exist.", position);
+			}
+
+			return filePath;
+		}
+
 		private static Value _cdinput (Compiler compiler, Variable[] variables, ValueRecipe[] values) {
 			string path;
 
@@ -96,17 +114,7 @@ namespace Spp {
 		private static Value _input (Compiler compiler, Variable[] variables, ValueRecipe[] values) {
 			string filePath;
 
-			Value val = values[0].Evaluate(compiler);
-
-			try {
-				filePath = Path.Combine(compiler.CdInput, val.AsString());
-			} catch (ArgumentException e) {
-				throw new CompileException("Illegal characters in path.", val.Position, e);
-			}
-
-			if (!File.Exists(filePath)) {
-				throw new CompileException("File does not exist.", val.Position);
-			}
+			filePath = _resolvePath(compiler.CdInput, values[0].Evaluate(compiler).AsString(), values[0].Position, false);
 
 			compiler.CompileInsert(filePath);
 			return new Text(default(Position), filePath);
@@ -134,13 +142,7 @@ namespace Spp {
 		private static Value _output (Compiler compiler, Variable[] variables, ValueRecipe[] values) {
 			string filePath;
 
-			Value val = values[0].Evaluate(compiler);
-
-			try {
-				filePath = Path.Combine(compiler.CdOutput, val.AsString());
-			} catch (ArgumentException e) {
-				throw new CompileException("Illegal characters in path.", val.Position, e);
-			}
+			filePath = _resolvePath(compiler.CdInput, values[0].Evaluate(compiler).AsString(), values[0].Position, true);
 
 			// qlikview requires its xml files to be utf8-bom -> UTF8Encoding(true)
 			compiler.Writer = new StreamWriter(filePath, false, new UTF8Encoding(true));
@@ -157,6 +159,72 @@ namespace Spp {
 			int i2 = values[1].Evaluate(compiler).AsInt();
 
 			return new Num(default(Position), i1 + i2);
+		}
+
+		private static Value _if (Compiler compiler, Variable[] variables, ValueRecipe[] values) {
+			bool b1 = values[0].Evaluate(compiler).AsBool();
+
+			if (b1) {
+				return values[1].Evaluate(compiler);
+			}
+			return Value.Empty;
+		}
+
+		private static Value _always (Compiler compiler, Variable[] variables, ValueRecipe[] values) {
+			return new Bool(default(Position), true);
+		}
+
+		private static Value _never (Compiler compiler, Variable[] variables, ValueRecipe[] values) {
+			return new Bool(default(Position), false);
+		}
+
+		private static Value _equals (Compiler compiler, Variable[] variables, ValueRecipe[] values) {
+			Value v1 = values[0].Evaluate(compiler);
+			Value v2 = values[1].Evaluate(compiler);
+
+			return new Bool(default(Position), v1.ToString() == v2.ToString());
+		}
+
+		private static Value _loadText (Compiler compiler, Variable[] variables, ValueRecipe[] values) {
+			string filePath;
+			StreamReader reader;
+			string contents;
+
+			filePath = _resolvePath(compiler.CdInput, values[0].Evaluate(compiler).AsString(), values[0].Position, false);
+
+			reader = new StreamReader(filePath);
+			contents = reader.ReadToEnd().Trim();
+			reader.Dispose();
+
+			return new Text(default(Position), contents);
+		}
+
+		private static Value _loadJson (Compiler compiler, Variable[] variables, ValueRecipe[] values) {
+			string filePath;
+			Reader reader;
+			Position position;
+			Value res;
+
+			filePath = _resolvePath(compiler.CdInput, values[0].Evaluate(compiler).AsString(), values[0].Position, false);
+
+			reader = new Reader(new StreamReader(filePath), filePath);
+			position = reader.Position;
+			switch (reader.Peek()) {
+				case '{': {
+					res = MapRecipe.Parser.Parse(reader).Evaluate(compiler);
+					reader.Dispose();
+					return res;
+				}
+				case '[': {
+					res = SequenceRecipe.Parser.Parse(reader).Evaluate(compiler);
+					reader.Dispose();
+					return res;
+				}
+				default: {
+					reader.Dispose();
+					throw new CompileException("Expected Json.", position);
+				}
+			}
 		}
 	}
 }
