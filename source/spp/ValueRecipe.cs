a597bc5046c1d62541369eec6ecda3a31ab5c053
Markus <m.olt@blaumond.net>
2018-11-28 08:31:52 +0100

    Allow values grouped in paranthesis.

--- source/spp/ValueRecipe.cs
+++ source/spp/ValueRecipe.cs
@@ -6,7 +6,8 @@ namespace Spp {
 	internal abstract class ValueRecipe {
 		protected Position _position;
 
-		internal static readonly Parser<ValueRecipe> ValueRecipeParser = new ParseGroup<ValueRecipe>("value", new Parser<ValueRecipe>[] {Num.Parser, Text.Parser, Command.Parser, Variable.Parser, MapRecipe.Parser, SequenceRecipe.Parser});
+		internal static readonly Parser<ValueRecipe> GroupParser = new ParseToken<ValueRecipe>("(", "(", _groupParser);
+		internal static readonly Parser<ValueRecipe> ValueRecipeParser = new ParseGroup<ValueRecipe>("value", new Parser<ValueRecipe>[] {GroupParser, Num.Parser, Text.Parser, Command.Parser, Variable.Parser, MapRecipe.Parser, SequenceRecipe.Parser});
 
 		protected ValueRecipe (Position position) {
 			_position = position;
@@ -21,5 +22,16 @@ namespace Spp {
 		}
 
 		internal abstract Value Evaluate (Compiler compiler);
+
+		private static ValueRecipe _groupParser (Reader reader) {
+			ValueRecipe res;
+
+			reader.Read();
+			reader.Skip(" \t\n");
+			res = ValueRecipeParser.Parse(reader);
+			reader.Skip(" \t\n");
+			reader.Assert(')');
+			return res;
+		}
 	}
 }
